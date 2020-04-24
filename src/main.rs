5e3d7ede58d9fccf239e8d146ebe224284d3af52
valentinpi <valentinpickel@gmx.de>
2020-04-25 00:21:00 +0200

    Refactored the code and implemented some more instructions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use rand::prelude::*;
-use sdl2::keyboard::Keycode;
-use sdl2::{audio, event, render, video};
-use std::{env, fs, io};
+use sdl2::{audio, event, keyboard::Keycode, render, video};
+use std::{collections, env, fs, io};
 
 const CHIP8_SCREEN_WIDTH: usize = 64;
 const CHIP8_SCREEN_HEIGHT: usize = 32;
@@ -16,12 +14,13 @@ struct Chip8 {
     dt: u8, // Delay timer
     st: u8, // Sound timer
     program: Vec<u8>,
+    key_bindings: collections::HashMap<Keycode, usize>,
     key_pad: [bool; 16],
 }
 
 impl Chip8 {
     pub fn new(program: Vec<u8>) -> Chip8 {
-        let chip8 = Chip8 {
+        let mut chip8 = Chip8 {
             ram: [0; 0x1000],
             pc: 0,
             ar: 0,
@@ -29,8 +28,26 @@ impl Chip8 {
             dt: 0,
             st: 0,
             program,
+            key_bindings: collections::HashMap::new(),
             key_pad: [false; 16],
         };
+
+        chip8.key_bindings.insert(Keycode::Num0, 0x0);
+        chip8.key_bindings.insert(Keycode::Num1, 0x1);
+        chip8.key_bindings.insert(Keycode::Num2, 0x2);
+        chip8.key_bindings.insert(Keycode::Num3, 0x3);
+        chip8.key_bindings.insert(Keycode::Num4, 0x4);
+        chip8.key_bindings.insert(Keycode::Num5, 0x5);
+        chip8.key_bindings.insert(Keycode::Num6, 0x6);
+        chip8.key_bindings.insert(Keycode::Num7, 0x7);
+        chip8.key_bindings.insert(Keycode::Num8, 0x8);
+        chip8.key_bindings.insert(Keycode::Num9, 0x9);
+        chip8.key_bindings.insert(Keycode::A, 0xA);
+        chip8.key_bindings.insert(Keycode::B, 0xB);
+        chip8.key_bindings.insert(Keycode::C, 0xC);
+        chip8.key_bindings.insert(Keycode::D, 0xD);
+        chip8.key_bindings.insert(Keycode::E, 0xE);
+        chip8.key_bindings.insert(Keycode::F, 0xF);
 
         return chip8;
     }
@@ -75,117 +92,24 @@ impl Chip8 {
                         break 'running;
                     }
                     KeyDown { keycode, .. } => {
-                        match keycode.unwrap() {
-                            Keycode::Num0 => {
-                                self.key_pad[0] = true;
+                        if keycode != None {
+                            let code = keycode.unwrap();
+                            match self.key_bindings.get(&code) {
+                                Some(binding) => self.key_pad[*binding] = true,
+                                None => {}
                             }
-                            Keycode::Num1 => {
-                                self.key_pad[1] = true;
-                            }
-                            Keycode::Num2 => {
-                                self.key_pad[2] = true;
-                            }
-                            Keycode::Num3 => {
-                                self.key_pad[3] = true;
-                            }
-                            Keycode::Num4 => {
-                                self.key_pad[4] = true;
-                            }
-                            Keycode::Num5 => {
-                                self.key_pad[5] = true;
-                            }
-                            Keycode::Num6 => {
-                                self.key_pad[6] = true;
-                            }
-                            Keycode::Num7 => {
-                                self.key_pad[7] = true;
-                            }
-                            Keycode::Num8 => {
-                                self.key_pad[8] = true;
-                            }
-                            Keycode::Num9 => {
-                                self.key_pad[9] = true;
-                            }
-                            Keycode::A => {
-                                self.key_pad[10] = true;
-                            }
-                            Keycode::B => {
-                                self.key_pad[11] = true;
-                            }
-                            Keycode::C => {
-                                self.key_pad[12] = true;
-                            }
-                            Keycode::D => {
-                                self.key_pad[13] = true;
-                            }
-                            Keycode::E => {
-                                self.key_pad[14] = true;
-                            }
-                            Keycode::F => {
-                                self.key_pad[15] = true;
-                            }
-                            _ => {}
                         }
-                        break 'running;
                     }
-
                     KeyUp { keycode, .. } => {
-                        match keycode.unwrap() {
-                            Keycode::Num0 => {
-                                self.key_pad[0] = false;
+                        if keycode != None {
+                            let code = keycode.unwrap();
+                            match self.key_bindings.get(&code) {
+                                Some(binding) => self.key_pad[*binding] = false,
+                                None => {}
                             }
-                            Keycode::Num1 => {
-                                self.key_pad[1] = false;
-                            }
-                            Keycode::Num2 => {
-                                self.key_pad[2] = false;
-                            }
-                            Keycode::Num3 => {
-                                self.key_pad[3] = false;
-                            }
-                            Keycode::Num4 => {
-                                self.key_pad[4] = false;
-                            }
-                            Keycode::Num5 => {
-                                self.key_pad[5] = false;
-                            }
-                            Keycode::Num6 => {
-                                self.key_pad[6] = false;
-                            }
-                            Keycode::Num7 => {
-                                self.key_pad[7] = false;
-                            }
-                            Keycode::Num8 => {
-                                self.key_pad[8] = false;
-                            }
-                            Keycode::Num9 => {
-                                self.key_pad[9] = false;
-                            }
-                            Keycode::A => {
-                                self.key_pad[10] = false;
-                            }
-                            Keycode::B => {
-                                self.key_pad[11] = false;
-                            }
-                            Keycode::C => {
-                                self.key_pad[12] = false;
-                            }
-                            Keycode::D => {
-                                self.key_pad[13] = false;
-                            }
-                            Keycode::E => {
-                                self.key_pad[14] = false;
-                            }
-                            Keycode::F => {
-                                self.key_pad[15] = false;
-                            }
-                            _ => {}
                         }
-                        break 'running;
                     }
-                    _ => {
-                        println!("EVENT");
-                    }
+                    _ => {}
                 }
             }
 
@@ -203,6 +127,7 @@ impl Chip8 {
             );
 
             match instruction {
+                // 00E0 - Clears the screen.
                 [0x0, 0x0, 0xE, 0x0] => {
                     let mut i = 0;
                     while i < NUM_PIXELS_BYTE {
@@ -211,29 +136,68 @@ impl Chip8 {
                     }
                     redraw = true;
                 }
+                // 00EE - Returns from a subroutine.
                 [0x0, 0x0, 0xE, 0xE] => {}
+                // 0NNN - Calls RCA 1802 program at address NNN. Not necessary for most ROMs.
                 [0x0, a, b, c] => {}
-                [0x1, a, b, c] => {}
-                [0x2, a, b, c] => {}
-                [0x3, a, b, c] => {}
-                [0x4, a, b, c] => {}
-                [0x5, a, b, c] => {}
-                [0x6, a, b, c] => {
-                    self.v[a as usize] = ((c << 4) | b) as u16;
+                // 1NNN - Jumps to address NNN.
+                [0x1, a, b, c] => {
+                    let mut addr: usize = 0;
+                    addr |= (a as usize) << 8;
+                    addr |= (b as usize) << 4;
+                    addr |= c as usize;
+                    self.pc = addr - 2;
                 }
-                [0x7, a, b, c] => {}
+                // 2NNN - Calls subroutine at NNN.
+                [0x2, a, b, c] => {}
+                // 3XNN - Skips the next instruction if VX equals NN. (Usually the next instruction is a jump to skip a code block)
+                [0x3, x, b, c] => {
+                    let mut val: u16 = 0;
+                    val |= (b as u16) << 4;
+                    val |= c as u16;
+                    if self.v[x as usize] == val {
+                        self.pc += 2;
+                    }
+                }
+                // 4XNN - Skips the next instruction if VX doesn't equal NN. (Usually the next instruction is a jump to skip a code block)
+                [0x4, x, b, c] => {
+                    let val = ((b << 4) | c) as u16;
+                    if self.v[x as usize] != val {
+                        self.pc += 2;
+                    }
+                }
+                // 5XNN - Skips the next instruction if VX equals VY. (Usually the next instruction is a jump to skip a code block)
+                [0x5, x, y, 0] => {
+                    if self.v[x as usize] == self.v[y as usize] {
+                        self.pc += 2;
+                    }
+                }
+                // 6XNN - Sets VX to NN.
+                [0x6, x, b, c] => {
+                    self.v[x as usize] = ((b << 4) | c) as u16;
+                }
+                // 7XNN - Adds NN to VX. (Carry flag is not changed)
+                [0x7, x, b, c] => {
+                    let val = ((b << 4) | c) as u16;
+                    self.v[x as usize] += val;
+                }
+                // 8XY0 - Sets VX to the value of VY.
                 [0x8, x, y, 0x0] => {
                     self.v[x as usize] = self.v[y as usize];
                 }
+                // 8XY1 - Sets VX to VX or VY. (Bitwise OR operation)
                 [0x8, x, y, 0x1] => {
                     self.v[x as usize] = self.v[x as usize] | self.v[y as usize];
                 }
+                // 8XY2 - Sets VX to VX and VY. (Bitwise AND operation)
                 [0x8, x, y, 0x2] => {
                     self.v[x as usize] = self.v[x as usize] & self.v[y as usize];
                 }
+                // 8XY3 - Sets VX to VX xor VY.
                 [0x8, x, y, 0x3] => {
                     self.v[x as usize] = self.v[x as usize] ^ self.v[y as usize];
                 }
+                // 8XY4 - Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there isn't.
                 [0x8, x, y, 0x4] => {
                     let s = self.v[x as usize] as u32 + self.v[y as usize] as u32;
                     if s < std::u16::MAX as u32 {
@@ -244,6 +208,7 @@ impl Chip8 {
                         self.v[x as usize] = (s & 0xFFFF) as u16;
                     }
                 }
+                // 8XY5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                 [0x8, x, y, 0x5] => {
                     let s: i32 = self.v[x as usize] as i32 - self.v[y as usize] as i32;
                     if s > 0 {
@@ -254,10 +219,12 @@ impl Chip8 {
                         self.v[x as usize] = -s as u16;
                     }
                 }
+                // 8XY6 - Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
                 [0x8, x, y, 0x6] => {
                     self.v[0xF] = self.v[x as usize] & 0x1;
                     self.v[x as usize] >>= 1;
                 }
+                // 8XY7 - Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't.
                 [0x8, x, y, 0x7] => {
                     let s: i32 = self.v[y as usize] as i32 - self.v[x as usize] as i32;
                     if s > 0 {
@@ -268,38 +235,43 @@ impl Chip8 {
                         self.v[x as usize] = -s as u16;
                     }
                 }
+                // 8XYE - Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
                 [0x8, x, y, 0xE] => {
                     self.v[0xF] = self.v[x as usize] & 0x8000;
                     self.v[x as usize] <<= 1;
                 }
+                // 9XY0 - Skips the next instruction if VX doesn't equal VY.
                 [0x9, x, y, 0x0] => {
-                    // Skips the next instruction if VX doesn't equal VY.
                     if self.v[x as usize] != self.v[y as usize] {
                         self.pc += 2;
                     }
                 }
+                // ANNN - Sets I to the address NNN.
                 [0xA, a, b, c] => {
-                    let mut result: u16 = 0;
-                    result |= (a as u16) << 8;
-                    result |= (b as u16) << 4;
-                    result |= c as u16;
-                    self.ar = result;
+                    let mut addr: u16 = 0;
+                    addr |= (a as u16) << 8;
+                    addr |= (b as u16) << 4;
+                    addr |= c as u16;
+                    self.ar = addr;
                 }
+                // BNNN - Jumps to the address NNN plus V0.
                 [0xB, a, b, c] => {
-                    let mut result: usize = 0;
-                    result |= (a as usize) << 8;
-                    result |= (b as usize) << 4;
-                    result |= c as usize;
-                    result += self.v[0] as usize;
-                    self.pc = result;
+                    let mut addr: usize = 0;
+                    addr |= (a as usize) << 8;
+                    addr |= (b as usize) << 4;
+                    addr |= c as usize;
+                    addr += self.v[0] as usize;
+                    self.pc = addr;
                 }
+                // CXNN - Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN.
                 [0xC, x, b, c] => {
-                    let mut result: u16 = 0;
-                    result |= (b as u16) << 4;
-                    result |= c as u16;
-                    result &= rand::random::<u16>();
-                    self.v[x as usize] = result;
+                    let mut addr: u16 = 0;
+                    addr |= (b as u16) << 4;
+                    addr |= c as u16;
+                    addr &= rand::random::<u16>();
+                    self.v[x as usize] = addr;
                 }
+                // DXYN - Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels. Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction. As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
                 [0xD, x, y, c] => {
                     // TODO: Implement this efficiently
                     // Watched this naming convention in a tutorial once
@@ -310,7 +282,9 @@ impl Chip8 {
                     //    yy += 1;
                     //}
                 }
+                // EX9E - Skips the next instruction if the key stored in VX is pressed. (Usually the next instruction is a jump to skip a code block)
                 [0xE, x, 0x9, 0xE] => {}
+                // EXA1 - Skips the next instruction if the key stored in VX isn't pressed. (Usually the next instruction is a jump to skip a code block)
                 [0xE, x, 0xA, 0x1] => {}
                 [0xF, x, 0x0, 0x7] => {}
                 [0xF, x, 0x0, 0xA] => {}
